f7bde3874ac588f6370fd78b60ba22807f8c99c0
Danichka13 <b.d.v.02@mail.ru>
2024-10-06 13:33:10 +0400

    commit changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-// Пока пусто. Добавьте скрипты по мере необходимости.
+// Этот скрипт раскрывает/сворачивает меню при клике (для мобильных устройств)
+document.querySelector('.dropbtn').addEventListener('click', function() {
+    let dropdownContent = document.querySelector('.dropdown-content');
+    dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+});
